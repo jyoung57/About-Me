4d91b7ca6047dde6b3c16c0ca7af738c7dcd3580
Jon Young <jey6639@gmail.com>
2023-06-03 23:33:46 -0500

    added js file and css style

--- aboutme.js
+++ aboutme.js
@@ -4,34 +4,47 @@ alert(`${yourName} welcome to my page`);
 
 let college = confirm('Did you go to college?');
 if (college === true) {
-  console.log('So did I');
+  alert('So did I');
+  // console.log('So did I');
 } else {
-  console.log('It is not for everyone.');
+  alert('It is not for everyone');
+  // console.log('It is not for everyone.');
 }
 
 let sport = confirm('Did you play a sport?');
 if (sport === true) {
-  console.log('I played football myself');
+  // console.log('I played football myself');
+  alert('I played football myself');
 } else {
-  console.log('Oh.  Well football is my favorite sport.');
+  // console.log('Oh.  Well football is my favorite sport.');
+  alert('Oh.  Well football is my favorite sport.');
+}
+
+let height = confirm('Are you shorter than 6 foot?');
+if (height === true) {
+  alert('I am taller than you.');
+  // console.log('I am taller than you.');
+} else {
+  alert('You must be pretty tall, sheesh.');
+  // console.log('You must be pretty tall, sheesh.');
 }
 
 let age = prompt('How old do you think I am?');
 
 switch (age.toLowerCase()) {
   case '41':
-    console.log('That is correct!!!');
-    // alert('That is correct!!!');
+    // console.log('That is correct!!!');
+    alert('That is correct!!!');
     break;
   case '40':
-    console.log('Oh, so close');
-    // alert('Oh, so close')
+    // console.log('Oh, so close');
+    alert('Oh, so close');
     break;
   case '25':
-    console.log('Bless your heart');
-    // alert('Bless your heart');
+    // console.log('Bless your heart');
+    alert('Bless your heart');
     break;
   default:
-    console.log(
-      `Your guess, ${age}, was incorrect.  The correct answer is 41.`);
+    alert(`Your guess, ${age}, was incorrect.  The correct answer is 41`);
+  // console.log(`Your guess, ${age}, was incorrect.  The correct answer is 4`);
 }
